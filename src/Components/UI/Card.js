8a6b9a7f39b477dd2dbf1d78f649fbf900e54166
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-10-21 10:44:37 +0200

    Adding Re-usable Card Component with CSS Module

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,4 +1,6 @@
-import "./Card.css";
-const Card = (props) => {};
+import styles from "./Card.module.css";
+const Card = (props) => {
+  return <div className={styles.card}>{props.children}</div>;
+};
 
 export default Card;
